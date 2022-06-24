39e386b7c434cbf5cfcd91671dce8b0882928a4f
root <alisontalatu@gmail.com>
2022-06-24 11:06:20 +0000

    ss

--- backend/routes/file.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-const multer  = require('multer');
-var router = express.Router();
-
-const FileController = require('../controllers/file.controller');
-const upload = multer({ dest: './files/' })
-
-router.post('/upload', upload.single('file'), FileController.uploadFile);
-module.exports = router;
